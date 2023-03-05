f7d0530fd92c09751a4f0ddebd18a1954aa78f7b
coding-streams <akxhdev@outlook.com>
2023-03-05 23:22:06 +0530

    add 01_create_database.sql

---
+++ 01_create_database.sql
@@ -0,0 +1,13 @@
+-- 1. View all databases.
+show databases;
+
+-- 2. Create a database named "test_db".
+create database test_db;
+show databases;
+
+-- 3. Select a database named "test_db".
+use test_db;
+
+-- 4. Delete a database named "test_db".
+drop database test_db;
+show databases;
