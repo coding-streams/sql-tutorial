2736a2f074fd89122b4aea65942071d73423bdbd
coding-streams <akxhdev@outlook.com>
2023-03-07 19:56:24 +0530

    add 04_insert_data.sql

---
+++ 04_insert_data.sql
@@ -0,0 +1,12 @@
+-- Single Row Insertion
+-- INSERT INTO employees(employee_id,first_name,email,hire_date,salary) 
+-- VALUES (1,'Michael','michael@mail.com','2022-04-13',84630);
+
+-- Multiple Rows Insertion
+INSERT INTO employees(employee_id,first_name,last_name,email,hire_date,salary) 
+VALUES (2,'Trevor','Philips','trevor@mail.com','2022-04-21',25447),
+(3,'Franklin','Clinton','franklin@mail.com','2022-04-21',30880),
+(4,'Wade','Hebert','wade@mail.com','2022-06-13',83835),
+(5,'Lester','Crest','lester@mail.com','2022-07-21',30586),
+(6,'Devin','Weston','devin@mail.com','2022-08-27',58948),
+(7,'Solomon','Richards','solomon@mail.com','2022-08-27',57935);
