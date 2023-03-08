e88347435b659f15e08bf288f32db4b56c3d2640
coding-streams <akxhdev@outlook.com>
2023-03-08 09:18:26 +0530

    06_query_data.sql

---
+++ 06_query_data.sql
@@ -0,0 +1,14 @@
+-- 1. Write a query to retrieve all the data from the "employees" table.
+
+select * 
+from employees;
+
+-- 2. Write a query to retrieve the first and last names of all employees from the "employees" table.
+
+select first_name, last_name 
+from employees;
+
+-- 3. Write a query to retrieve the employee_id, first name, last name and email of all employees from the "employees" table.
+
+select employee_id, first_name, last_name, email
+from employees;
